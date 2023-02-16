c93fdeeb5713f4a5c1db20e7b63050fc7bb05d04
Mahmoud sabry <76252661+Mahmoudshoaib@users.noreply.github.com>
2023-02-16 14:31:33 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.sass";
+import avatar from "./images/image-jeremy.png";
+import data from "./data.json";
+import work from "./images/icon-work.svg";
+import play from "./images/icon-play.svg";
+import study from "./images/icon-study.svg";
+import exercise from "./images/icon-exercise.svg";
+import social from "./images/icon-social.svg";
+import selfCare from "./images/icon-self-care.svg";
+import ellipsis from "./images/icon-ellipsis.svg";
+import { useEffect, useState } from "react";
+const list = [work, play, study, exercise, social, selfCare];
 
 function App() {
+  const [timingType, setTimingType] = useState("weekly");
+
+  const handleTimingTypeChange = (e) => {
+    setTimingType(e.target.value);
+  };
+
+  useEffect(() => {
+    const btns = document.querySelectorAll(".time-tracking-type input");
+    btns.forEach(
+      (btn) =>
+        (btn.className = `btn ${btn.value === timingType ? "active" : ""}`)
+    );
+  }, [timingType]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="time-tracking-dashboard">
+      <div className="container">
+        <div className="span-2 main">
+          <div className="user-info">
+            <div className="image">
+              <img alt="..." src={avatar} />
+            </div>
+            <p>Report for</p>
+            <h2 className="name">Jeremy Robson</h2>
+          </div>
+          <div className="time-tracking-type">
+            <input
+              type="button"
+              className="btn"
+              value={"daily"}
+              onClick={handleTimingTypeChange}
+            />
+            <input
+              type="button"
+              className="btn"
+              value={"weekly"}
+              onClick={handleTimingTypeChange}
+            />
+            <input
+              type="button"
+              className="btn"
+              value={"monthly"}
+              onClick={handleTimingTypeChange}
+            />
+          </div>
+        </div>
+        {data.map((card, i) => {
+          return (
+            <div className={`card card-${i} ${card.title}`} key={card.title}>
+              <div className="logo">
+                <img src={list[i]} alt="..." />
+              </div>
+              <div className="content">
+                <p className="title">
+                  {card.title}
+                  <img src={ellipsis} alt="..." />
+                </p>
+                <h1 className="current-hours">
+                  {card.timeframes[timingType].current}hrs
+                </h1>
+                <p className="prev-hours">
+                  Last Week - {card.timeframes[timingType].previous}hrs
+                </p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
